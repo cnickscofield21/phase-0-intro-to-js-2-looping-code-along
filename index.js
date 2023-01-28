a75a7ea7545e0ceefab4d52a7cd3418b31aca67f
Nick Scofield <c.nick.scofield@gmail.com>
2023-01-28 11:24:42 -0700

    commit tree ce42400980e105d1b7d2215d2e57eebb592c3f7a

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i += 1) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+
+    return gifts;
+}
+
+const names = ["Charlie", "Samip", "Ali"];
+const occasion = "birthday";
+
+function writeCards(names, occasion) {
+    const thankYous = [];
+
+    for (let i = 0; i < names.length; i += 1) {
+        thankYous.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`);
+    }
+
+    return thankYous;
+}
+
+function countDown(posInt = 10) {
+    /*// while version
+    while (posInt > -1) {
+        console.log(posInt);
+        posInt -= 1;
+    }
+    */
+
+    // for version
+    for (posInt; posInt > -1; posInt -= 1) {
+        console.log(posInt);
+    }
+}
+
+countDown();
